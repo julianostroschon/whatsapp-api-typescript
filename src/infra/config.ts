8e9a5ecfb5bb0d5d0306b018366559579aabe5af
julianostroschon <julianostroschon@gmail.com>
2024-05-16 14:52:32 -0300

    🧐 Configura o projeto

--- src/infra/config.ts
+++ src/infra/config.ts
@@ -1,11 +1,11 @@
-// import { z } from 'zod';
 import { config } from 'dotenv';
 config()
 
 export const env = Object.freeze({
-  CHAT_API_SECRET: process.env.CHAT_API_SECRET || 'segredo',
+  GROUP_TO_SEND_ERROR: process.env.GROUP_TO_SEND_ERROR || 'jojo',
   DEFAULT_RECEIVER: process.env.DEFAULT_RECEIVER || 'groupName',
-  TECH_LEAD: process.env.TECH_LEAD || '555596929042@c.us',
-  HOST: process.env.HOST || '0.0.0.0',
+  TECH_LEAD: process.env.TECH_LEAD || '5599992200@c.us',
+  CHAT_API_SECRET: process.env.SECRET || 'segredo',
   PORT: Number(process.env.PORT || '3001'),
+  HOST: process.env.HOST || '0.0.0.0',
 });
