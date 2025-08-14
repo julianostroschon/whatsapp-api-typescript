41909b9e3d7a20eac332421bdd0c0ab35643642c
julianostroschon <julianostroschon@gmail.com>
2025-08-14 15:57:58 -0300

    ♻️ Add importações faltantes

--- src/consumer/index.ts
+++ src/consumer/index.ts
@@ -1,5 +1,4 @@
 import { parentLogger } from '@/infra/logger';
-import { getClientOptions } from '@/services/whatsapp/lib';
 import 'module-alias/register';
 import { startRabbitConsumer } from './rabbit';
 
@@ -7,7 +6,6 @@ const logger = parentLogger.child({ service: 'consumer-app' });
 
 (async () => {
   await startRabbitConsumer();
-  getClientOptions('oito')
 })().catch((err) => {
   logger.error(err);
   process.exit(1);
--- src/services/index.ts
+++ src/services/index.ts
@@ -1,2 +1,3 @@
-export * from "./whatsapp";
+export * from "./rabbit";
+export * from "./telegram";
 
