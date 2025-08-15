d51aaa1fe5f5cc67f12ee7d4bbc4faaac24fb23f
julianostroschon <julianostroschon@gmail.com>
2025-08-15 11:03:13 -0300

    ♻️ Altera o path do pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,12 @@
+const path = require("node:path")
+
 const port = process.env.PORT || 3005
 const token = process.env.TELEGRAM_TOKEN || 'token'
 module.exports = {
   apps: [
     {
       name: "producer",
-      script: "dist/producer",
+      script: path.join(__dirname, "dist/producer"),
       env: {
         "PORT": port,
         "TELEGRAM_TOKEN": token,
