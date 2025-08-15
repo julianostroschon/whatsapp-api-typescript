a8b8a50c4aaee0fdb8e46b88592a80ea6114006b
julianostroschon <julianostroschon@gmail.com>
2025-08-15 16:32:52 -0300

    ✨ Add ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,18 +1,23 @@
-const { config } = require("dotenv")
-config()
-const path = require("node:path")
-
-const port = process.env.PORT || 3005
-const token = process.env.TELEGRAM_TOKEN || 'token'
 module.exports = {
   apps: [
     {
-      name: "producer",
-      script: path.join(__dirname, "dist/producer"),
-      env: {
-        "PORT": port,
-        "TELEGRAM_TOKEN": token,
-      }
+      name: 'producer',
+      script: './dist/producer', // Caminho para o seu arquivo compilado
+      interpreter: 'node',
+      // Você pode usar o env_file ou referenciar as variáveis diretamente.
+      // Opção 1: Usar um arquivo .env
+      env_file: '.env', // PM2 vai procurar por este arquivo na raiz do projeto
+      // Opção 2: Definir as variáveis diretamente
+      // env: {
+      //   NODE_ENV: 'development',
+      //   VARIAVEL_A: 'valor_a',
+      //   VARIAVEL_B: 'valor_b',
+      // },
+      // Opção 3: Usar arquivos de ambiente diferentes para cada ambiente
+      // env_production: {
+      //   NODE_ENV: 'production',
+      //   PORT: 80,
+      // },
     },
-  ]
+  ],
 }
