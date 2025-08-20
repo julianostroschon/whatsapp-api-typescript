0f922f2e077cd5c66a3e5a092854dd00f913d8ee
julianostroschon <julianostroschon@gmail.com>
2025-08-19 22:07:48 -0300

    ✨ Atualizado service http

---
+++ src/services/http/index.ts
@@ -0,0 +1,26 @@
+import { cfg } from "@/infra/config";
+import { parentLogger } from "@/infra/logger";
+import fastify, { FastifyInstance } from "fastify";
+import { constructRoutes } from "./routes";
+
+export async function buildFastify(): Promise<FastifyInstance> {
+  const app = fastify();
+  const logger = parentLogger.child({ service: 'API' });
+
+  // Adicionar uma rota de teste simples
+  app.get('/', async () => {
+    return { hello: 'world' };
+  });
+
+  await constructRoutes(app, logger);
+
+  app.listen({ port: cfg.PORT }, (err) => {
+    if (err) {
+      logger.error(err);
+      process.exit(1);
+    }
+    logger.info(`🌐 Server listening on port:${cfg.PORT}`);
+  });
+
+  return app;
+}
