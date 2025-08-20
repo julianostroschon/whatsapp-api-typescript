657ffa0e4424e82259a3784b43f2c90658c2337e
julianostroschon <julianostroschon@gmail.com>
2025-08-19 22:24:40 -0300

    🧑‍💻 Atualiza teste e pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,23 +2,10 @@ module.exports = {
   apps: [
     {
       name: 'producer',
-      script: '/dist/producer', // Caminho para o seu arquivo compilado
-      cwd: __dirname, // Adicione esta linha
+      script: '/dist/producer',
+      cwd: __dirname,
       interpreter: 'node',
-      // Você pode usar o env_file ou referenciar as variáveis diretamente.
-      // Opção 1: Usar um arquivo .env
-      env_file: '.env', // PM2 vai procurar por este arquivo na raiz do projeto
-      // Opção 2: Definir as variáveis diretamente
-      // env: {
-      //   NODE_ENV: 'development',
-      //   VARIAVEL_A: 'valor_a',
-      //   VARIAVEL_B: 'valor_b',
-      // },
-      // Opção 3: Usar arquivos de ambiente diferentes para cada ambiente
-      // env_production: {
-      //   NODE_ENV: 'production',
-      //   PORT: 80,
-      // },
+      env_file: __dirname + '/.env',
     },
   ],
 }
